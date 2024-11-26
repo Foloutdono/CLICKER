a1c87f6fe826ef863299651f4d8e16c2377cdf43
Foloutdono <125478727+Foloutdono@users.noreply.github.com>
2024-11-27 00:15:34 +0100

    Add files via upload

--- GAMER.js
+++ GAMER.js
@@ -1,26 +1,37 @@
 let nbFLOPS = 0;
 let nbClicks = 0;
-let incrementValue = 1;
-let cartes = [];
+let clickValue = 1;
+let fallingObjects = [];
+let upgrades = {};
+let components = {};
 let divScore;
 let divNbFLOPS;
 let divFallingGPUS;
+let divFlopsPerSec;
 let clicker;
 let divRack;
+let divUpgrades;
 function Component(name, value, price) {
 	this.name = name;
 	this.nb = 0;
 	this.value = value;
-	this.price = price;
+	this.currentPrice = price;
 	this.multiplier = 1;
 	this.is_dislay = false;
 	this.button = document.getElementById(`${name}B`);
 	this.flopParSecComp = function() {
 		return this.nb * this.value * this.multiplier;
 	};
-	this.newPrice = function () {
-		this.price = Math.floor(this.price * 1.15);
+	this.newPrice = function (nb = 1, selling = false) {
+		this.currentPrice = this.price(nb, selling);
 	};
+	this.price = function (nb = 1, selling = false) {
+		if (!selling) {
+			return Math.floor(this.currentPrice * Math.pow(1.15, nb));
+		} else {
+			return Math.floor(this.currentPrice * Math.pow(0.7, nb));
+		}
+	}
 	this.createButton = function () {
 		img = document.createElement("img");
 		img.src = `assets/${this.name}.png`;
@@ -33,7 +44,7 @@ function Component(name, value, price) {
 		name.textContent = this.name;
 		price = document.createElement("div");
 		price.classList.add("buyBPrice");
-		price.textContent = this.price;
+		price.textContent = this.currentPrice;
 		nb = document.createElement("div");
 		nb.classList.add("buyBNb");
 		nb.textContent = this.nb;
@@ -44,32 +55,76 @@ function Component(name, value, price) {
 		this.button.addEventListener("click", buyComp(this.name, 1));
 	};
 	this.updateButton = function () {
-		console.log(this.button.children[2].textContent, this.price);
-		this.button.children[2].textContent = this.price;
+		this.button.children[2].textContent = this.currentPrice;
 		this.button.children[3].textContent = this.nb;
 	};
 }
+function Upgrade(name, comp, price, value, type, conditions) {
+	this.name = name;
+	this.comp = comp;
+	this.price = price;
+	this.type = type;
+	this.value = value;
+	this.is_for_sell = false;
+	this.is_sold = false;
+	this.conditions = conditions;
+	this.button = document.getElementById(`up`);
 
-let components = {};
+	this.buy = function (up, nb=1) {
+		return () => {
+			if (up.price <= nbFLOPS) {
+				nbFLOPS -= up.price;
+				up.is_for_sell = false;
+				up.is_sold = true;
+
+				components[up.comp].multiplier *= value;
+			}
+		};
+	};
+	this.createButton = function () {
+		this.button.addEventListener("click", this.buy(this));
+	};
+	this.respect_conditions = function () {
+		if (conditions !== undefined) {
+			let iCond = 0;
+			let is_respected = true;
+			while (iCond < conditions.length && is_respected) {
+				is_respected = eval(conditions[iCond]);
+				iCond++;
+			}
+			return is_respected;
+		} else {
+			return true;
+		}
+	};
+}
+
 function init() {
 	fillComponents();
+	fillUpgrades();
 	divScore = document.getElementById("score");
 	divNbFLOPS = document.getElementById("nbFLOPS");
 	clicker = document.getElementById("clicker");
 	divFallingGPUS = document.getElementById("fallingGPUS");
 	divRack = document.getElementById("rack");
+	divName = document.getElementById("name");
+	divFlopsPerSec = document.getElementById("flopsPerSec");
+	divUpgrades = document.getElementById("upgrades");
+
 
 	clicker.addEventListener("click", clickOnClicker);
+	divName.addEventListener("click", getValue(divName, "What's your name ?"));
 
 	fillRack();
 
 	setInterval(update, 30)
 	setInterval(updateFlops, 1000);
 }
+window.onload = init;
 function buyComp(comp, nb) {
 	return () => {
-		if (components[comp].price <= nbFLOPS) {
-			nbFLOPS -= components[comp].price;
+		if (components[comp].currentPrice <= nbFLOPS) {
+			nbFLOPS -= components[comp].currentPrice;
 			components[comp].nb += nb;
 			for (let i = 0; i < nb; i++) {
 				components[comp].newPrice();
@@ -77,7 +132,7 @@ function buyComp(comp, nb) {
 			img = document.createElement("img");
 			img.src = `assets/${comp}.png`;
 			iDeck = 0;
-			while(iDeck < divRack.children.length && divRack.children[iDeck].id != comp) {
+			while(iDeck < divRack.children.length && divRack.children[iDeck].id != comp + "R") {
 				iDeck += 2;
 			}
 			if (iDeck < divRack.children.length) {
@@ -87,8 +142,19 @@ function buyComp(comp, nb) {
 		}
 	};
 }
+function getValue(element, message) {
+	return function () {
+		showPrompt(message, function (response) {
+			if (response === null) {
+				console.log("User canceled the prompt.");
+			} else {
+				element.textContent = response;
+			}
+		});
+	}
+}
 function clickOnClicker() {
-	nbFLOPS += incrementValue;
+	nbFLOPS += clickValue;
 	nbClicks++;
 
 	effectsClick();
@@ -108,7 +174,7 @@ function effectsClick() {
 	newGPU.style.transform = `rotate(${getRandomInt(150)}deg)`;
 	newGPU.style.opacity = 1;
 
-	cartes.push(newGPU);
+	fallingObjects.push(newGPU);
 	divFallingGPUS.appendChild(newGPU);
 
 	newGPU.interval = setInterval(falling(newGPU));
@@ -124,9 +190,9 @@ function falling(GPU) {
 function suprGPU(GPU) {
 	return () => {
 		clearInterval(GPU.interval);
-		let index = cartes.indexOf(GPU);
+		let index = fallingObjects.indexOf(GPU);
 		if (index > -1) { 
-		  cartes.splice(index, 1);
+		  fallingObjects.splice(index, 1);
 		}
 		GPU.remove();
 	};
@@ -136,6 +202,8 @@ function getRandomInt(max) {
 }
 function update() {
 	updateNbFLOPS();
+	updateFlopsPerSec();
+	updateAvailableComps();
 }
 function updateNbFLOPS() {
 	divNbFLOPS.textContent = nbFLOPS % 1 != 0 ? nbFLOPS.toFixed(1): nbFLOPS;
@@ -145,11 +213,27 @@ function updateFlops() {
 		nbFLOPS += components[comp].flopParSecComp();
 	}
 }
+function updateFlopsPerSec() {
+	let flopsPerSec = 0;
+	for (let comp in components) {
+		flopsPerSec += components[comp].flopParSecComp();
+	}
+	divFlopsPerSec.textContent = flopsPerSec % 1 != 0 ? flopsPerSec.toFixed(1): flopsPerSec;
+}
+function updateAvailableComps() {
+	for (let comp in components) {
+		if (components[comp].currentPrice > nbFLOPS) {
+			components[comp].button.classList.add("unAffordable");
+		} else {
+			components[comp].button.classList.remove("unAffordable");
+		}
+	}
+}
 function fillRack() {
 	for (let comp in components) {
 		newDeck = document.createElement("div");
 		newDeck.classList.add("deck");
-		newDeck.id = comp;
+		newDeck.id = comp + "R";
 		
 		divRack.appendChild(newDeck);
 		sep = document.createElement("div");
@@ -158,11 +242,38 @@ function fillRack() {
 	}
 }
 function fillComponents() {
-	components["gamer"] = new Component("gamer", 0.1, 15)
-	components["gpu"] = new Component("gpu", 2, 10000)
+	components["gamer"] = new Component("gamer", 0.1, 15);
+	components["gpu"] = new Component("gpu", 2, 100);
+	components["rack"] = new Component("rack", 25, 1000);
 
 	for(let comp in components) {
 		components[comp].createButton();
 	}
 }
-window.onload = init;
+function fillUpgrades() {
+	upgrades["truc"] = new Upgrade("truc", "gpu", 1, 2, "type", "this.price == 1");
+
+	for(let up in upgrades) {
+		upgrades[up].createButton();
+	}
+}
+function showPrompt(message, callback) {
+	const promptOverlay = document.getElementById("customPrompt");
+	const promptMessage = document.getElementById("promptMessage");
+	const promptInput = document.getElementById("promptInput");
+
+	promptMessage.textContent = message;
+	promptInput.value = ""; // Reset the input field
+	promptOverlay.classList.remove("hidden");
+
+	submitPrompt = function () {
+	const userInput = promptInput.value;
+		promptOverlay.classList.add("hidden");
+		if (callback) callback(userInput);
+	};
+
+	closePrompt = function () {
+		promptOverlay.classList.add("hidden");
+		if (callback) callback(null);
+	};
+}
